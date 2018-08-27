b2dcd7d105b2cbbab66aed73e86bbce17a0a0820
y-kisse <15102288+y-kisse@users.noreply.github.com>
2018-08-27 22:52:42 +0900

    [update] settings for REST Framework and OAuth toolkit

--- django_oauth_test/settings.py
+++ django_oauth_test/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oauth2_provider',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+    )
+}
 
 
 # Internationalization
